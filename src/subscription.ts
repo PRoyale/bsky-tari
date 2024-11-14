097b5cde3c3006c114a3017d4482628211404886
Pyriel <61353556+PRoyale@users.noreply.github.com>
2024-11-14 13:35:54 +0100

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -21,7 +21,18 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const postsToCreate = ops.posts.creates
       .filter((create) => {
         // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
+        return create.record.text.toLowerCase().includes('tari')
+            || create.record.text.toLowerCase().includes('tarifanart')
+            || create.record.text.toLowerCase().includes('metarunner')
+            || create.record.text.toLowerCase().includes('smg4tari')
+            || create.record.text.toLowerCase().includes('smg4tarifanart')
+            || create.record.text.toLowerCase().includes('tarismg4')
+            || create.record.text.toLowerCase().includes('tariplush')
+            || create.record.text.toLowerCase().includes('tariplushy')
+            || create.record.text.toLowerCase().includes('tariplushie')
+            || create.record.text.toLowerCase().includes('metarunnertari')
+            || create.record.text.toLowerCase().includes('tarimetarunner')
+            || create.record.text.toLowerCase().includes('metarunnertarifanart')
       })
       .map((create) => {
         // map alf-related posts to a db row
